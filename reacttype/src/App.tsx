0bae8b8bc8cbc098146faaa45c364bf45c5932e4
ilkeratar <74013091+ilkeratar@users.noreply.github.com>
2023-05-31 21:28:44 +0300

    react-typescript

---
+++ reacttype/src/App.tsx
@@ -0,0 +1,59 @@
+import React, { ChangeEvent, FC, useState } from "react";
+import "./App.css";
+import {todoType} from './apptypes';
+import TodoItem from "./TodoItem";
+
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [workDay, setWorkDay] = useState<number>(0);
+  const [todoList, setTodoList] = useState<todoType[]>([]);
+  
+  const handleChange=(event:ChangeEvent<HTMLInputElement>)=>{
+    if(event.target.name==="task"){
+      setTask(event.target.value);
+    } else{
+      setWorkDay(Number(event.target.value));
+    }
+  }
+  const addNewTask=(): void=>{
+    const newTask={taskName:task,workDay};
+    setTodoList([...todoList,newTask]);
+    setTask("");
+    setWorkDay(0);
+  }
+
+  const deleteTask=(nameToDelete:string):void=>{
+    setTodoList(todoList.filter((task)=>{
+      return task.taskName !== nameToDelete;
+    }));
+  };
+
+  return (
+    <div className="App">
+      <div className="mainCard">
+        <input className="mainCardInput"
+        type="text" 
+        name="task"
+        value={task} 
+        placeholder="Enter new task" 
+        onChange={handleChange}/>
+        <input className="mainCardInput"
+          type="number"
+          name="workDay"
+          value={workDay}
+          placeholder="Number of days to complete"
+          onChange={handleChange}/>
+        <button className="mainCardButton" onClick={addNewTask}>Add a new task</button>
+      </div>
+      <div className="todoCard">
+        {todoList.map((task:todoType,index:number)=>{
+          return(
+            <TodoItem key={index} task={task} deleteTask={deleteTask}/>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
